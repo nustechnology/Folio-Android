1fcaa23e21b8333d888c694d796e1ccb733b2c48
namnn <namnn@nustechnology.com>
2026-09-24 16:30:40 +0700

    style(source): italicize title in source detail header

--- app/src/main/java/com/nus/folio/presentation/sourcedetail/SourceDetailHeader.kt
+++ app/src/main/java/com/nus/folio/presentation/sourcedetail/SourceDetailHeader.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -96,6 +97,7 @@ internal fun SourceDetailHeader(
                 fontFamily = CormorantGaramond,
                 fontSize = 28.sp,
                 fontWeight = FontWeight.SemiBold,
+                fontStyle = FontStyle.Italic,
                 color = HomeHeader,
                 modifier = Modifier.weight(1f),
                 maxLines = 2,
